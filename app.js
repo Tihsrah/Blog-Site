5387b4f94119788dd295c93874f535332bd7f966
Tihsrah <harshlf4@gmail.com>
2022-06-24 17:02:40 +0530

    Blog site almost done. with only new post no css glitch

--- app.js
+++ app.js
@@ -1,16 +1,69 @@
 const express= require('express');
 const bodyParser=require('body-parser');
+var _ = require('lodash');
+
+
 const app=express();
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.set('view engine','ejs');
+app.listen('4000',function(){});
 
-app.listen('3000',function(){});
+// variables
+var dict = {
+};
+var tit="";
+var story="";
+posts=[]
 
-app.get('/test',function(req,res){
-  res.render("index");
+
+app.get('/',function(req,res){
+  res.render("index",{dict:dict});
 })
-app.post('/',function(req,res){
-  res.send("working");
+
+
+app.get('/about',function(req,res){
+  res.render('about');
+})
+app.get('/contact',function(req,res){
+  res.render('contact');
+})
+
+app.get('/compose',function(req,res){
+  res.render('compose',{title:tit,message:story});
+})
+app.post('/compose',function(req,res){
+   tit=req.body.title;
+   story=req.body.textarea;
+  dict[`${tit}`]=story;
+  // console.log(dict);
+  posts.push(tit);
+  // console.log(posts);
+  res.redirect('/');
+})
+
+app.get('/same-bro-same',function(req,res){
+  res.render('extra');
+})
+
+//yahan par kam kar rhe the
+app.get('/posts/:postName',function(req,res){
+  var userAsked=req.params.postName;
+  userAsked=_.lowerCase(userAsked);
+  var flag=0;
+  for (const k in dict){
+    var storedTitle=_.lowerCase(k)
+    if(storedTitle===userAsked){
+      flag=1;
+      break;
+    }
+  }
+  if(flag===1){
+    // console.log(posts);
+  res.render('extra',{post:userAsked,dict:dict})
+}else{
+  res.render('notFound');
+}
 })
