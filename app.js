19bb44d93569e82741305ee2e1a51c4af96d0e3b
Tihsrah <harshlf4@gmail.com>
2022-06-27 16:57:51 +0530

    All bug fixed!

--- app.js
+++ app.js
@@ -1,69 +1,78 @@
-const express= require('express');
-const bodyParser=require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 var _ = require('lodash');
 
 
-const app=express();
+const app = express();
 
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('public'));
-app.set('view engine','ejs');
-app.listen('4000',function(){});
+app.set('view engine', 'ejs');
+app.listen('4000', function() {});
 
 // variables
-var dict = {
-};
-var tit="";
-var story="";
-posts=[]
+var dict = {};
+var tit = "";
+var story = "";
+posts = []
 
 
-app.get('/',function(req,res){
-  res.render("index",{dict:dict});
+app.get('/', function(req, res) {
+  res.render("index", {
+    dict: dict
+  });
 })
 
 
-app.get('/about',function(req,res){
+app.get('/about', function(req, res) {
   res.render('about');
 })
-app.get('/contact',function(req,res){
+app.get('/contact', function(req, res) {
   res.render('contact');
 })
 
-app.get('/compose',function(req,res){
-  res.render('compose',{title:tit,message:story});
+app.get('/compose', function(req, res) {
+  res.render('compose', {
+    title: tit,
+    message: story
+  });
 })
-app.post('/compose',function(req,res){
-   tit=req.body.title;
-   story=req.body.textarea;
-  dict[`${tit}`]=story;
+app.post('/compose', function(req, res) {
+  tit = req.body.title;
+  story = req.body.textarea;
+  dict[`${tit}`] = story;
   // console.log(dict);
   posts.push(tit);
   // console.log(posts);
   res.redirect('/');
 })
 
-app.get('/same-bro-same',function(req,res){
+app.get('/same-bro-same', function(req, res) {
   res.render('extra');
 })
 
 //yahan par kam kar rhe the
-app.get('/posts/:postName',function(req,res){
-  var userAsked=req.params.postName;
-  var temp=_.lowerCase(userAsked);
-  var flag=0;
-  for (const k in dict){
-    var storedTitle=_.lowerCase(k)
-    if(storedTitle===temp){
-      flag=1;
+app.get('/posts/:postName', function(req, res) {
+  var userAsked = req.params.postName;
+  var temp = _.lowerCase(userAsked);
+  var flag = 0;
+  for (const k in dict) {
+    var storedTitle = _.lowerCase(k)
+    if (storedTitle === temp) {
+      flag = 1;
       break;
     }
   }
-  if(flag===1){
+  if (flag === 1) {
     // console.log(posts);
-  res.render('extra',{post:userAsked,dict:dict})
-}else{
-  res.render('notFound');
-}
+    res.render('extra', {
+      post: userAsked,
+      dict: dict
+    })
+  } else {
+    res.render('notFound');
+  }
 })
