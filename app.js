10b8800749ba8d67abe54ee26d94c216b214166b
Tihsrah <harshlf4@gmail.com>
2022-06-28 20:06:05 +0530

    working app with one error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose=require("mongoose");
 var _ = require('lodash');
 
 
@@ -14,16 +15,40 @@ app.set('view engine', 'ejs');
 app.listen('4000', function() {});
 
 // variables
-var dict = {};
+// var dict = {};
 var tit = "";
 var story = "";
-posts = []
+// posts = []
+
+//mongo
+//mongodb://localhost:27017/blog
+//
+mongoose.connect("mongodb+srv://Tihsrah:harshlf4@cluster0.mzqbbfb.mongodb.net/blogDB",{useNewUrlParser:true});
+
+const blogsSchema={
+  blogTitle:String,
+  blogBody:String
+}
+const Blog=mongoose.model("Blog",blogsSchema);
 
 
+
+const s="";
 app.get('/', function(req, res) {
-  res.render("index", {
-    dict: dict
-  });
+
+  Blog.find({},function(err,foundBlog){
+      // console.log("Success");
+      if(err){
+
+      }
+      res.render("index", {s:s,foundItem:foundBlog});
+      
+  })
+
+
+  // res.render("index", {
+  //   dict: dict
+  // });
 })
 
 
@@ -40,12 +65,20 @@ app.get('/compose', function(req, res) {
     message: story
   });
 })
+
 app.post('/compose', function(req, res) {
   tit = req.body.title;
   story = req.body.textarea;
-  dict[`${tit}`] = story;
+
+  // dict[`${tit}`] = story;
+  const blog=new Blog({
+    blogTitle:tit,
+    blogBody:story
+  })
+  blog.save();
+
   // console.log(dict);
-  posts.push(tit);
+  // posts.push(tit);
   // console.log(posts);
   res.redirect('/');
 })
@@ -56,23 +89,43 @@ app.get('/same-bro-same', function(req, res) {
 
 //yahan par kam kar rhe the
 app.get('/posts/:postName', function(req, res) {
-  var userAsked = req.params.postName;
-  var temp = _.lowerCase(userAsked);
-  var flag = 0;
-  for (const k in dict) {
-    var storedTitle = _.lowerCase(k)
-    if (storedTitle === temp) {
-      flag = 1;
-      break;
-    }
-  }
-  if (flag === 1) {
-    // console.log(posts);
-    res.render('extra', {
-      post: userAsked,
-      dict: dict
+  var userAsked = mongoose.Types.ObjectId(req.params.postName);
+  userAsked=String(userAsked)
+  // var temp = _.lowerCase(userAsked);
+  // var flag = 0;
+  // console.log(userAsked);
+    Blog.findById(userAsked,function(err,foundList){
+      // console.log(foundList.blogTitle)
+      if(err){
+        // console.log("lol");
+      }else{
+      res.render('extra', {post: foundList.blogTitle,dict: foundList.blogBody,del:userAsked});
+      }
     })
-  } else {
-    res.render('notFound');
-  }
+  })
+
+app.post('/delete',function(req,res){
+  const toBeDeleted=req.body.delete;
+  Blog.findByIdAndRemove({_id:toBeDeleted},function(err){
+
+  });
+  res.redirect('/');
 })
+
+  // for (const k in dict) {
+  //   var storedTitle = _.lowerCase(k)
+  //   if (storedTitle === temp) {
+  //     flag = 1;
+  //     break;
+  //   }
+  // }
+  // if (flag === 1) {
+  //   // console.log(posts);
+  //   res.render('extra', {
+  //     post: userAsked,
+  //     dict: dict
+  //   })
+  // } else {
+  //   res.render('notFound');
+  // }
+// })
